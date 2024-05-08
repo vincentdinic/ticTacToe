f1dab54840bec5ac40ae9bd39d82680062d443bc
Vincent Di Nicolantonio <vincentdinic@gmail.com>
2024-05-08 00:53:06 -0400

    init files

---
+++ index.js
@@ -0,0 +1,17 @@
+// basic script for tic tac toe game vincent di nicolantonio
+
+// init
+
+// while ! hasWon
+
+// whose turn it is (x starts)
+
+// event listener to see what square is selected
+
+// is valid square
+
+// check for win
+
+// if true, show play again button
+
+// boom done
