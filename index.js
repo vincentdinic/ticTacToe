f5a4a5d6302cceac190643cce3688d9e977ed523
Vincent Di Nicolantonio <vincentdinic@gmail.com>
2024-05-08 01:39:09 -0400

    created game board!

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 // basic script for tic tac toe game vincent di nicolantonio
 
-// init
+// function to make board
+function createBoard(){
+    const board = [
+        ["11", "12", "13"], 
+        ["21", "22", "23"], 
+        ["31", "32", "33"] 
+        ];
+    const table = document.getElementById("gameBoard");
+    for (let i = 0; i < 3; i++){
+        const tr = document.createElement('tr');
+        for (let j = 0; j < 3; j++){
+            const td = document.createElement('td');
+            td.textContent = board[i][j];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    return table;
+}
 
+// init
+function init(){
+    createBoard();
+    
+}
 // while ! hasWon
 
 // whose turn it is (x starts)
